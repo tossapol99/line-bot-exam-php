3d34e19820ece848c6fd2376fcc2dfb399be6592
tossapol99 <32153399+tossapol99@users.noreply.github.com>
2020-12-30 00:45:29 +0700

    user code goragod

--- send_message.php
+++ send_message.php
@@ -2,12 +2,25 @@
 require "vendor/autoload.php";
 
 $access_token = 'sQIhQpJuigcattjZwq6w7Oc+lfFkFarRlGwNWGkTE7R0yZr1IiFtjStWqN52NGxQzFkUtxBnoQsEUTOp4rcaZY+NxB1+aRS7c3NgsODk16TIFeqNbMUZzVK7S4EJ4WTWiLn6awsB0x760+oawwbFAFGUYhWQfeY8sLGRXgo3xvw=';
+$userId = 'Uc2b199a976d4926471571a28e3713aec';
 
-$channelSecret = '24dbd4589ca06c84af3e61a2128c19f0';
-$idPush = 'Uc2b199a976d4926471571a28e3713aec'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+// ข้อความที่ต้องการส่ง
+$messages = array(
+    'type' => 'text',
+    'text' => 'ทดสอบการส่งข้อความ',
+);
+$post = json_encode(array(
+    'to' => array($userId),
+    'messages' => array($messages),
+));
+// URL ของบริการ Replies สำหรับการตอบกลับด้วยข้อความอัตโนมัติ
+$url = 'https://api.line.me/v2/bot/message/multicast';
+$headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+echo $result;
